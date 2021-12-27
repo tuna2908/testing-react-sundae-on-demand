68746b14b60c6837c4d144db08267ff97751e88b
ohohohjkl <anhtuck01@gmail.com>
2021-12-27 11:07:14 +0700

    popover validation - both asyncronization

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form, OverlayTrigger, Popover } from "react-bootstrap";
 
 export const SummaryForm = () => {
   const [cbStatus, setCBStatus] = useState(false);
@@ -20,18 +20,29 @@ export const SummaryForm = () => {
   );
 };
 
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+  </Popover>
+);
+
 export const BootstrapSummaryForm = () => {
   const [cbStatus, setCBStatus] = useState(false);
   const handleCBClick = (e) => setCBStatus(e.target.checked);
   //wanna add label to checkbox => add this one: <Form.Group controlId="terms-and-conditions">
+  //placement="right" is important
+  const Label = (
+    <span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
   return (
     <Form>
       <Form.Group controlId="terms-and-conditions">
-        <Form.Check
-          type="checkbox"
-          onChange={handleCBClick}
-          label={"I agree to Terms and Conditions"}
-        />
+        <Form.Check type="checkbox" onChange={handleCBClick} label={Label} />
       </Form.Group>
       <button disabled={!cbStatus}>Confirm order</button>
     </Form>
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,12 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  screen,
+  waitForElementToBeRemoved,
+  waitFor,
+} from "@testing-library/react";
 import { SummaryForm, BootstrapSummaryForm } from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 const getTestingElements = () => {
   render(<BootstrapSummaryForm />);
@@ -7,7 +14,8 @@ const getTestingElements = () => {
   const termCheckbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
-  return { summitButton, termCheckbox };
+  const termSpan = screen.getByText(/terms and conditions/i);
+  return { summitButton, termCheckbox, termSpan };
 };
 
 test("Initial state of Checkbox and Button", () => {
@@ -28,4 +36,51 @@ test("Disable button works!", () => {
   fireEvent.click(termCheckbox);
   //check button enable
   expect(summitButton).toBeDisabled();
+});
+
+test("USER-EVENT Disable button works!", () => {
+  const { summitButton, termCheckbox } = getTestingElements();
+
+  //click checkbox
+  userEvent.click(termCheckbox);
+  //check if button summit disabled
+  expect(summitButton).toBeEnabled();
+  //unclick
+  userEvent.click(termCheckbox);
+  //check button enable
+  expect(summitButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const { termSpan } = getTestingElements();
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  //check pop over not even rendered yet
+  expect(nullPopover).not.toBeInTheDocument();
+
+  //hover the term
+  userEvent.hover(termSpan);
+  //check pop over existence
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument(); //best practices to leave the code here for the readability
+
+  //unhover
+  userEvent.unhover(termSpan);
+
+  //hope that popover disappear
+  const nullPopoverAgain = screen.queryByText(
+    //check again because component disappeard, and new component need new search
+    /no ice cream will actually be delivered/i
+  );
+  setTimeout(() => {
+    //a way to fix async update (DOM update after the tests - may be for css duration, animation, etc.)
+    expect(nullPopoverAgain).not.toBeInTheDocument();
+  }, 500);
+
+  await waitFor(() => {
+    expect(nullPopoverAgain).not.toBeInTheDocument();
+  });
 });
